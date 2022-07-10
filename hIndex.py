4231b250ca61bfd9cb0d054db9f618658b5924d0
zbluee <99397149+zbluee@users.noreply.github.com>
2022-07-10 16:29:49 -0400

    hIndex

---
+++ hIndex.py
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/h-index/
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse = True)
+        for i, c in enumerate(citations):
+            if i >= c:
+                return i
+        return len(citations)
+       
+        
+        
