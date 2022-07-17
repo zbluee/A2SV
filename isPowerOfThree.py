e714c425f6a1f128a0c433eb65ae4fc0475ab752
zbluee <99397149+zbluee@users.noreply.github.com>
2022-07-17 14:26:43 -0400

    pow3

---
+++ isPowerOfThree.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/power-of-three/
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n > 1:
+            while n % 3 == 0:
+                n /= 3
+        return n == 1
+            
+        
+        
