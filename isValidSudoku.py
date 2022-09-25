da9c833256deab706b93fb04ac36c04040fed19d
zbluee <99397149+zbluee@users.noreply.github.com>
2022-09-25 14:51:03 -0400

    Valid Sudoku

---
+++ isValidSudoku.py
@@ -0,0 +1,20 @@
+#Question -> https://leetcode.com/problems/valid-sudoku/
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        sub_matrix = defaultdict(set)
+        
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in sub_matrix[(r//3,c//3)] :
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                sub_matrix[(r//3, c//3)].add(board[r][c])
+        return True
+            
+            
+        
