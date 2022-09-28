de70ecf93a6e67525522a626279e10067aaba1f0
zbluee <99397149+zbluee@users.noreply.github.com>
2022-09-28 14:28:49 -0400

    isPalindrome

---
+++ ValidPalindrome.py
@@ -0,0 +1,17 @@
+#Question -> https://leetcode.com/problems/valid-palindrome/
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        while left < right:
+            
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() == s[right].lower():
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
+            
